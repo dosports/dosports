7e42f358a53e0bdb5a6f6ef5389ce3c307676531
mingsound21 <mingsound@kakao.com>
2022-07-02 17:24:59 +0900

    product image 추가

--- front/productList/productList.js
+++ front/productList/productList.js
@@ -21,15 +21,14 @@ $clothes_image_page_container.addEventListener('click', (event) =>{
     const target = event.target;
     const page_spans = target.parentNode.children;
     for(let i=0; i<page_spans.length;i++){
-        page_spans[i].style.fontWeight = '400';
+        page_spans[i].classList.remove('page_check');
     }
     if(target.tagName != 'SPAN') return;
-    target.style.fontWeight = '600';
+    target.classList.add('page_check');
+    changeClothImage();
 })
 
-window.addEventListener('load', (event)=>{
-    $clothes_image_page_container.children[0].style.fontWeight = '600';
-})
+
 
 // filter page click _ check sign
 function removeCheckSign(dom, className){
@@ -69,13 +68,58 @@ $price_container.addEventListener('click', (event)=>{
 $detail_category = document.querySelector('.detail_category');
 $detail_category.addEventListener('click', (event) =>{
     addClassToChoose($detail_category, 'choose', event, 'SPAN');
+    changeClothImage();
 })
 
-// expert opinion
+// expert opinion open & close
 const $expert_opinion_arrow = document.querySelector('.expert_opinion_arrow');
 const $expert_opinion_main = document.querySelector('.expert_opinion_main');
 
 $expert_opinion_arrow.addEventListener('click',(event) => {
     $expert_opinion_main.classList.toggle('expert_opinion_show');
     $expert_opinion_arrow.classList.toggle('expert_opinion_arrow_up');
+})
+
+// change cloth image
+// const cloth_detail_category = ["상의", "팬츠", "스커트", "운동화", "스포츠용품"];
+const $cloth_images = document.querySelectorAll('.cloth_image');
+console.log($cloth_images);
+const cloth_images = {
+    '상의' :
+    ["https://image.nbkorea.com/NBRB_Product/20220627/NB20220627124135358001.jpg", 
+    'https://image.nbkorea.com/NBRB_Product/20220627/NB20220627124201146001.jpg',
+    'https://image.nbkorea.com/NBRB_Product/20220627/NB20220627124148594001.jpg'],
+    '팬츠' :
+    ["https://image.nbkorea.com/NBRB_Product/20220624/NB20220624142359153001.jpg", 
+    'https://image.nbkorea.com/NBRB_Product/20220624/NB20220624142426181001.jpg',
+    'https://image.nbkorea.com/NBRB_Product/20220624/NB20220624142412883001.jpg'],
+    '스커트' :
+    ["https://image.nbkorea.com/NBRB_Product/20220523/NB20220523150748131001.jpg", 
+    'https://image.nbkorea.com/NBRB_Product/20220329/NB20220329163014985001.jpg',
+    'https://image.nbkorea.com/NBRB_Product/20220120/NB20220120105038056001.jpg'],
+    '운동화' :
+    ["https://image.nbkorea.com/NBRB_Product/20220608/NB20220608160147835001.jpg", 
+    'https://image.nbkorea.com/NBRB_Product/20220629/NB20220629092520275001.jpg',
+    'https://image.nbkorea.com/NBRB_Product/20220629/NB20220629174428397001.jpg'],
+    '스포츠용품' :
+    ["https://cdn3-aka.makeshop.co.kr/shopimages/jo112/0210000000383.jpg?1647239036", 
+    'https://cdn3-aka.makeshop.co.kr/shopimages/jo112/0210000000123.jpg?1639716730',
+    'https://cdn3-aka.makeshop.co.kr/shopimages/jo112/0210000000173.jpg?1639714567']
+};
+const cloth_categorys = Object.keys(cloth_images);
+function changeClothImage(){
+    const cloth_cate = $detail_category.querySelector('.choose').innerText;
+    const page_num = $clothes_image_page_container.querySelector('.page_check').innerText;
+    const imageSrc = cloth_images[cloth_cate];
+    for(let i=0;i<$cloth_images.length;i++){
+        $cloth_images[i].children[0].src = imageSrc[page_num-1];
+    }
+    
+}
+
+// on load
+window.addEventListener('load', (event)=>{
+    $clothes_image_page_container.children[0].classList.add('page_check');
+    $detail_category.children[0].classList.toggle('choose');
+    changeClothImage();
 })
